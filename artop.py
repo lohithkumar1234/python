9e6fb2519e0312d0fd10d478033814315a766e1e
lohithkumar1234 <lohithkumarambati@gmail.com>
2026-09-03 12:47:25 +0530

    Add files via upload

---
+++ artop.py
@@ -0,0 +1,37 @@
+print("hello world")
+print("malla reddy demeed to university")
+#arthemartic operation
+a=10
+b=3
+print("adittion:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor division:", a//b)
+print("remainder:",a % b)
+print(" power:",a**b)
+salary =25000
+ha=salary*0.20
+dfa=salary*0.10
+total_salary=salary +ha+dfa
+print("Total salar:", total_salary)
+a=int(input("Enter frist number"))
+b=int(input("Enter second number"))
+print("addition",a+b)
+print("subtraction:", a-b)
+print ("multiplication:",a*b )
+print("division:",a/b )
+# student mark claculate
+name = input("Enter student name" )
+m1 = int(input("enter python mark:"))
+m2 = int(input("Enter java mark:"))
+m3 = int(input("enter sql marks:"))
+
+total =m1+m2+m3
+average =total/3
+
+print("\n---------student report---------")
+print("name:", name)
+print("total:", total)
+
+
---
+++ bank.py
@@ -0,0 +1,31 @@
+# assirment operation 
+x=10
+
+x += 5
+print(x)
+
+
+#bank balance
+balance=10000
+deposit= 5000
+balance += deposit 
+print("after deposit:", balance)
+withdraw =2000
+balance -= withdraw
+print("after widthdraw:", balance)
+
+#comparison operators
+a=20
+b=40
+print(a==b)
+print (a!= b)
+print(a>b)
+print(a<b)
+print(a>= b)
+print(a<= b)
+#age eligiblity checker
+age = int(input("enter your age"))
+print("eligible:", age >= 18)
+
+
+
